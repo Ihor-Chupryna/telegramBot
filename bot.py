4ac0070332da16d15c82796cf4427cc6326d7e05
Ihor-Chupryna <igchup1985@gmail.com>
2024-01-20 13:48:46 +0200

    quest bot with inline button

--- bot.py
+++ bot.py
@@ -30,59 +30,48 @@ def create_user(message):
     if user_id not in user_data:
         user_data[user_id] = {}
         user_data[user_id]['username'] = message.from_user.first_name
-        # user_data[user_id]['test_points'] = 0
         save_user_data(user_data, data_path)
         print(user_id)
         print(user_data)
     elif user_id in user_data:
         bot.send_message(message.chat.id, f"{message.from_user.first_name} рад вас снова видеть")
 
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    keyboard.add(telebot.types.KeyboardButton('Kingdom Light'))
-    keyboard.add(telebot.types.KeyboardButton('Kingdom Dark'))
-    # keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    # keyboard.add(KeyboardButton('Kingdom Light'))
-    # keyboard.add(KeyboardButton('Kingdom Dark'))
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton('Kingdom Light', callback_data='kingdom_light'))
     keyboard.add(InlineKeyboardButton('Kingdom Dark', callback_data='kingdom_dark'))
 
-
-    bot.send_message(message.chat.id, f"Choose a kingdom: Kingdom Light or Kingdom Dark", reply_markup=keyboard)
-
-
-@bot.message_handler(func=lambda message: message.text == "Kingdom Light")
-def location_one(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    keyboard.add(telebot.types.KeyboardButton('Forest'))
-    keyboard.add(telebot.types.KeyboardButton('Desert'))
-    bot.send_message(message.chat.id, f"Welcome to {locations['kingdom_light']['name']}")
-    print(locations['kingdom_light']['image'])
-    bot.send_photo(message.chat.id, open(locations["kingdom_light"]["image"], "rb"))
-
-    bot.send_message(message.chat.id, f"Kingdom Light. Choose a location: Forest or Desert", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Choose a kingdom:", reply_markup=keyboard)
 
 
-@bot.message_handler(func=lambda message: message.text == "Forest")
-def location_forest(message):
-    bot.send_message(message.chat.id, "Forest Hello")
-    bot.send_photo(message.chat.id, open(locations["forest"]["image"], "rb"))
+@bot.callback_query_handler(func=lambda call: call.data in ["kingdom_light", "kingdom_dark"])
+def location_selected(call):
+    # user_id = str(call.from_user.id)
+    if call.data == "kingdom_light":
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(InlineKeyboardButton('Forest', callback_data='forest'))
+        keyboard.add(InlineKeyboardButton('Desert', callback_data='desert'))
+
+        bot.send_message(call.message.chat.id, f"Welcome to {locations['kingdom_light']['name']}")
+        bot.send_photo(call.message.chat.id, open(locations["kingdom_light"]["image"], "rb"))
+        bot.send_message(call.message.chat.id, "Kingdom Light. Choose a location:", reply_markup=keyboard)
+    elif call.data == "kingdom_dark":
+        bot.send_message(call.message.chat.id, "Welcome to Kingdom Dark")
+        bot.send_photo(call.message.chat.id, open(locations["kingdom_dark"]["image"], "rb"))
+        say_finish(call.message.chat.id)
 
 
-@bot.message_handler(func=lambda message: message.text == "Desert")
-def location_sahara(message):
-    bot.send_message(message.chat.id, "Desert Hello")
-    bot.send_photo(message.chat.id, open(locations["desert"]["image"], "rb"))
+@bot.callback_query_handler(func=lambda call: call.data in ["forest", "desert"])
+def location_selected(call):
+    if call.data == "forest":
+        bot.send_message(call.message.chat.id, "Forest Hello")
+        bot.send_photo(call.message.chat.id, open(locations["forest"]["image"], "rb"))
+    elif call.data == "desert":
+        bot.send_message(call.message.chat.id, "Desert Hello")
+        bot.send_photo(call.message.chat.id, open(locations["desert"]["image"], "rb"))
 
 
-@bot.message_handler(func=lambda message: message.text == "Kingdom Dark")
-def location_two(message):
-    bot.send_message(message.chat.id, "Kingdom Dark")
-    bot.send_photo(message.chat.id, open(locations["kingdom_dark"]["image"], "rb"))
-    say_finish(message.chat.id)
-
-@bot.message_handler()
 def say_finish(chat_id):
-    bot.send_message(chat_id, "finish game")
+    bot.send_message(chat_id, "Finish game")
+
 
 bot.polling()
